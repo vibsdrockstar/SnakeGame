308bc0b7a6ee82d3e0e690c4920a8ebfdd095141
Vibhor <bijoy.vibhor19@gmail.com>
2016-06-14 17:01:12 +0530

    Create point.java

---
+++ point.java
@@ -0,0 +1,31 @@
+
+public class point {
+
+private int x,y;
+public point(){
+x=0;
+y=0;
+
+}
+public point(int x,int y){
+	this.x=x;
+	this.y=y;
+	
+}
+public void setX(int x){
+	this.x=x;
+	
+}
+public void setY(int y){
+	this.y=y;
+	
+}
+public int getX(){
+	return x;
+	
+}
+public int getY(){
+	return y;
+	
+}
+}
